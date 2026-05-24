cd3ba21c0c472a5f4c8c57dc3db76d0dc09c37dd
Jayther <hem.and.nas.and.rif@gmail.com>
2026-05-24 09:01:01 +0700

    auto update

--- pages/api/user/count.js
+++ pages/api/user/count.js
@@ -1,18 +1,26 @@
-import { connectToDB } from "@/utils/db";
-import User from "@/models/User";
+import { connectToDB } from "../../../utils/db";
+import User from "../../../models/User";
 
 export default async function handler(req, res) {
+
+  try {
+
     await connectToDB();
 
-    if (req.method === "GET") {
-        try {
-            const userCount = await User.countDocuments(); // ✅ นับจำนวน Users ทั้งหมด
-            res.status(200).json({ count: userCount });
-        } catch (error) {
-            console.error("Error fetching user count:", error);
-            res.status(500).json({ error: "Failed to fetch user count" });
-        }
-    } else {
-        res.status(405).json({ error: "Method Not Allowed" });
-    }
+    const count = await User.countDocuments();
+
+    return res.status(200).json({
+      count
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 }
