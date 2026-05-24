580ce72f8481ca42d77e4cdcb6edf36bf26f6a51
Jayther <hem.and.nas.and.rif@gmail.com>
2026-05-24 08:37:30 +0700

    auto update

--- models/User.js
+++ models/User.js
@@ -4,17 +4,19 @@ const UserSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     email: { type: String },
-    points: { type: Number, default: 0 }, // ✅ Point ของผู้ใช้
+    points: { type: Number, default: 0 },
     products: [
         {
-            productId: { type: mongoose.Schema.Types.ObjectId, ref: "Item" }, // ✅ ID สินค้าที่ซื้อ
+            productId: { type: mongoose.Schema.Types.ObjectId, ref: "Item" },
             name: { type: String },
             image: { type: String },
             version: { type: String },
             fileUrl: { type: String },
-            purchaseDate: { type: Date, default: Date.now }, // ✅ วันเวลาที่ซื้อ
+            purchaseDate: { type: Date, default: Date.now },
+            discordRoleIds: { type: [String], default: [] }, // ✅ เพิ่มฟิลด์นี้
         }
     ],
+    loginAt: { type: Date, default: Date.now }, // ✅ เพิ่ม loginAt (optional)
 });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
